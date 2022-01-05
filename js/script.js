4bbc1555e5fd56a91ee36e515a9901a158377553
timrusin <timrusin@gmail.com>
2022-01-05 00:24:10 -0600

    Player logic, computer timer, player clicks

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
 const playerTiles = document.querySelectorAll(".player-tile");
-const compTiles = document.querySelectorAll(".comp-tile");
+const compTiles = document.querySelectorAll(".comp-tile")
 
-// console.log(compTiles)
-let pLives = "0";//corosponding to the score taly
-let cLives = "0";//corosponding to the score taly
-const hiding= "HIDE";//simply holding the string that will be displayed
-const seeking= "SEEK";//simply holding the string that will be displayed
-let computerHiding = []//computer's hiding spots
-let playerHiding = []//player's hiding spots
-let computerGuesses = []//To keep track of the computer's guesses so it wont pick the same index twice
-let playerGuesses = [] //not sure this arrauy will be neccesary yet
-const playerBoard = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-const computerBoard = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+//Variables
+let pLives = 0;  //coresponding to the score taly
+let cLives = 0;  //coresponding to the score taly
+const hiding = "HIDE";  //simply holding the string that will be displayed
+const seeking = "SEEK";  //simply holding the string that will be displayed
+const computerHiding = [];  //computer's hiding spots
+const playerHiding = [];  //player's hiding spots
+const computerGuesses = [];  //To keep track of the computer's guesses so it wont pick the same index twice
+const playerGuesses = []; //not sure this array will be necessary yet
+const playerBoard = [];
+const computerBoard = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];  //.fill for array(look up)
+let turn = "Player"
 
-//Elements
+//DOM Elements
 const playerLives = document.getElementById("player-lives");
 const cpuLives = document.getElementById("cpu-lives");
 const display = document.getElementById("display-span"); 
@@ -24,76 +25,69 @@ const display = document.getElementById("display-span");
 // const clickSound = new Audio("")
 // const foundSound = new Audio("")
 
+//Player hide function
 playerTiles.forEach((tile) => tile.addEventListener("click", playerHides));
+compTiles.forEach((tile) => tile.addEventListener("click", clickTile));
 function playerHides(event){
-    display.style.fontSize= "35px";
+    display.style.fontSize= "35px";  //consider making a separate class
     display.innerText = "Player is hiding";
-    pLives++; 
-    playerLives.innerText= (pLives);
+    pLives++; //consider having a new function to break things up a bit more (separate display stuff into it's own fuctions)
+    playerLives.innerText= pLives;
     const tile = event.target;
-    const tileNumber = tile.dataset.index;
-    tile.innerText= hiding;
+    const tileNumber = tile.dataset.index;  //look into query for this attribute as opposed to event.target
+    tile.innerText= hiding;                  
     playerBoard[tileNumber] = hiding;
-        console.log(playerBoard); //this is pushing "hide" into our board on correct index!
-        if (pLives === 4){
-            return playerTiles.forEach((tile)=>tile.removeEventListener("click", playerHides)), computerHides()}
-        else if (tile.innerText != ""){
-            return tile.removeEventListener("click", playerHides);
-         }   
-    // console.log(playerHides)
+     console.log(playerBoard); //this is pushing "hide" into our board on correct index!
+    if (pLives === 4){
+        playerTiles.forEach((tile)=>tile.removeEventListener("click", playerHides))  //consider resetPlayer function to reset clicks
+        computerHides()
+    }else if (tile.innerText !== ""){
+        tile.removeEventListener("click", playerHides);  
+    }   
 }
 
 function computerHides(){
     display.classList.add('fade');
     display.innerText = "Computer is hiding";
-    cLives=4; //would be cool to have this count up slowly while computer "hides" (stretch goal)
-    cpuLives.innerText = cLives;
-    computerBoard.sort((a,b) => 0.5 - Math.random());
-    for (let i=0; i<4; i++){
+    computerBoard.sort((a,b) => 0.5 - Math.random());      
+
+    const compTimer = setInterval(() => {
         let spacePos = computerBoard.pop();
-        console.log(spacePos)
-        computerHiding.push(spacePos)
+        console.log(spacePos);
+        computerHiding.push(spacePos);
         compTiles[spacePos].innerText = hiding;
         compTiles[spacePos].classList.add('hide');
-        compTiles[spacePos].addEventListener('click', hit)
-    }
-    playerTurn()
+        cLives++;
+        cpuLives.innerText = cLives;
+        if (cLives === 4){
+            clearInterval(compTimer);
+            playerTurn();
+        }
+    }, 1000);
 }
-//my original process before buiilding it into the loop above
-    // let hiding1 = computerBoard.pop();
-    // let hiding2 = computerBoard.pop();
-    // let hiding3 = computerBoard.pop();
-    // let hiding4 = computerBoard.pop();
-    // compTiles[hiding1].innerText = hiding;
-    // compTiles[hiding2].innerText = hiding;
-    // compTiles[hiding3].innerText = hiding;
-    // compTiles[hiding4].innerText = hiding;
-    // compTiles[hiding1].classList.add('hide');
-    // compTiles[hiding2].classList.add('hide');
-    // compTiles[hiding3].classList.add('hide');
-    // compTiles[hiding4].classList.add('hide');
-    // let hit1 = compTiles[hiding1];
-    // hit1.addEventListener('click', hit);
-    // let hit2 = compTiles[hiding2];
-    // hit2.addEventListener('click', hit);
-    // let hit3 = compTiles[hiding3];
-    // hit3.addEventListener('click', hit);
-    // let hit4 = compTiles[hiding4];
-    // hit4.addEventListener('click', hit);
-
-
 
 function playerTurn(){
     display.classList.remove('fade');
     display.style.fontSize = "35px";
-    display.innerText = "Player's turn"
-    console.log(computerHiding)
-
+    display.innerText = "Player's turn";
+    turn = "Player";
+    console.log(computerHiding);
 }
-
+function clickTile(event){
+    const tile = event.target;
+    const tileNumber = parseInt(tile.dataset.index);
+    if (turn !== "Player") return;  //if it is the computers turn I wont be able to run the rest of the function
+    if (computerHiding.indexOf(tileNumber) === -1){
+        tile.innerText=seeking;
+    }else{ 
+        hit()
+    }
+     //index of is type sensative, had to parseInt tileNumber
+}
 function hit(){
     cLives--;
     cpuLives.innerText = cLives;
+    //need to change the display of the hidden piece, to show we've found the spot (sound) 
 }
 
 
