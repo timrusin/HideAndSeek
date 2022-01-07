eabd1e3c4d44daa85d146e2f1898977ff76cf6c7
timrusin <timrusin@gmail.com>
2022-01-07 13:57:11 -0600

    refactored, new lose music

--- js/script.js
+++ js/script.js
@@ -1,22 +1,9 @@
 //TODOs
-//- remove functions from functions
 //- Splash Screen with instructions
 //- more animation with hits and score
 //DONT FORGET TO UPDATE THE README PAGE - CHECK PROJECT CRITERIA FOR PRESENTATION!!
 
-//boad elements
-const playerTiles = document.querySelectorAll(".player-tile");
-const compTiles = document.querySelectorAll(".comp-tile");
-playerTiles.forEach((tile) => tile.addEventListener("click", playerHides));
-compTiles.forEach((tile) => tile.addEventListener("click", playerSearch));
-
-//game over elements
-const winner = document.querySelector(".winner");
-const loser = document.querySelector(".loser");
-const restartBtn = document.querySelectorAll(".restart-button");
-restartBtn.forEach((button) => button.addEventListener("click", restart))
-
-//Audio
+//Audio elements
 const click1 = new Audio("audio/click1.mp3");
 const demonHideSound = new Audio("audio/demonHide.wav");
 const playerFind = new Audio("audio/playerFind.mp3");
@@ -25,61 +12,84 @@ const compFind = new Audio("audio/compFind.m4a");
 const loseSound = new Audio("audio/demogorgon.mp3");
 const music = new Audio("audio/To Orlando 18.m4a");
 const winMusic = new Audio("audio/strangerThings.m4a");
+const loseMusic = new Audio("audio/loseMusic.wav")
 
-//variables
-let pLives = 0; 
-let cLives = 0;  
-const hiding = "HIDE";  
-const seeking = "SEEK";  
-let compHiding = [];  
-let playerHiding = [];  
-let compOptions = Array.from(Array(16).keys());
-let compGuesses = [];  
-let playerBoard = [];
-let compBoard = Array.from(Array(16).keys());
-let turn
-let helperTimeout
+//board elements
+const playerTiles = document.querySelectorAll(".player-tile");
+const compTiles = document.querySelectorAll(".comp-tile");
+playerTiles.forEach((tile) => tile.addEventListener("click", playerHidingClick));
+compTiles.forEach((tile) => tile.addEventListener("click", playerTurnSearch));
 
 //Taly board elements
 const playerLives = document.getElementById("player-lives");
 const cpuLives = document.getElementById("cpu-lives");
 const display = document.getElementById("display-span"); 
 
-//This is where the player chooses thier hidding spots
-function playerHides(event){
-    turn = "Player";
-    music.loop = true
-    music.play()
+//game over elements
+const winner = document.querySelector(".winner");
+const loser = document.querySelector(".loser");
+const restartBtn = document.querySelectorAll(".restart-button");
+restartBtn.forEach((button) => button.addEventListener("click", restart))
+
+//global variables
+let pLives = 0; 
+let cLives = 0;  
+const hiding = "HIDE";  
+const seeking = "SEEK"; 
+let turn
+let helperTimeout
+
+//board and guess arrays 
+let compHiding = []; 
+let playerHiding = [];  
+let compHidingOptions = Array.from(Array(16).keys());
+let compGuesses = [];   
+let playerBoard = [];
+let compBoard = Array.from(Array(16).keys());
+
+//functions
+//These two functions are where the player chooses their hidding spots
+function playerHidingClick(event){
+    const tile = event.target; 
+    click1.play();
+    const tileNumber = parseInt(tile.dataset.index);
+    tile.innerText= hiding;                 
+    playerHiding.push(tileNumber) 
+    playerHidingDisplay()              
+    music.loop = true;
+    music.play();
     music.volume = .5;
-    music.loop;
+        if (pLives === 4){
+            playerTiles.forEach((tile)=>tile.removeEventListener("click", playerHidingClick))  
+            computerHidingDisplay();
+        }else if (tile.innerText !== ""){
+            tile.removeEventListener("click", playerHidingClick);  
+        }   
+}
+
+function playerHidingDisplay(){
+    turn = "Player";
     display.style.fontSize= "35px";  
     display.innerText = "Player is hiding";
     pLives++;
     playerLives.innerText= pLives;
-    const tile = event.target;
-    const tileNumber = parseInt(tile.dataset.index);  
-    tile.innerText= hiding;                 
-    playerHiding.push(tileNumber) 
-    if (pLives === 4){
-        playerTiles.forEach((tile)=>tile.removeEventListener("click", playerHides))  
-        computerHides();
-    }else if (tile.innerText !== ""){
-        tile.removeEventListener("click", playerHides);  
-    }   
-    click1.play();
 }
 
-//This is where the computer chooses it's hidding spots
-function computerHides(){
+//These two functions are where the computer chooses it's hidding spots
+function computerHidingDisplay(){
     turn = "Computer";
     demonHideSound.play();
     display.classList.add('fade');
     display.innerText = "Demogorgon is hiding";
+    computerHidingClick();
+}
+
+function computerHidingClick(){
     compBoard.sort((a,b) => 0.5 - Math.random());      
     const hideInterval = setInterval(() => {
         click1.play();
-        let tile = compBoard.pop();
-        compHiding.push(tile);
+        let hidingSpot = compBoard.pop();
+        compHiding.push(hidingSpot);
         cLives++;
         cpuLives.innerText = cLives;
         if (cLives === 4){
@@ -90,45 +100,53 @@ function computerHides(){
     console.log(compHiding);
 }
 
-//this function is managing the players game play
+//these four functions are managing the players game play
 function playerTurnDisplay(){ 
     turn = "Player";
     display.classList.remove('fade');
     display.innerText = "Player's turn";
     console.log(compHiding);
     helperTimeout = setTimeout(helperMessage, 4000);
-    function helperMessage (){
-        display.classList.add('fade');
-        display.innerText= "click above";
-    }
 }
 
-function playerSearch(event){ 
+function helperMessage (){
+    display.classList.add('fade');
+    display.innerText= "click above";
+}
+
+function playerTurnSearch(event){ 
     if (turn !== "Player") return;  
-    const tile = event.target;
-    const tileNumber = parseInt(tile.dataset.index);
+    const hidingSpot = event.target;
+    const spotNumber = parseInt(hidingSpot.dataset.index);
     click1.play();
-    if (compHiding.indexOf(tileNumber) === -1){
-        tile.innerText=seeking;
-        tile.classList.add ('playerSeek');
+    if (compHiding.indexOf(spotNumber) === -1){
+        hidingSpot.innerText=seeking;
+        hidingSpot.classList.add ('playerSeek');
         computerTurnDisplay();
-        const computerTimeOut = setTimeout(computerSearch, 2000);
+        const computerTimeOut = setTimeout(computerTurnSearch, 2000);
     }else{ 
         cLives --;
         cpuLives.innerText = cLives;
-        tile.classList.add('c-found');
-        tile.innerText = "FOUND";
+        hidingSpot.classList.add('c-found');
+        hidingSpot.innerText = "FOUND";
         playerFind.play();
-        if (cLives === 0){
-            gameOverWin();
-        }else{
-            computerTurnDisplay();
-            const computerTimeOut = setTimeout(computerSearch, 2000);
-        }
+        //prevent double Guess
+        if (hidingSpot.innerText !== ""){
+            hidingSpot.removeEventListener("click", playerTurnSearch)};
+        checkForWin();
     }
 }
 
-//These functions are managing the computer's game play
+function checkForWin(){
+    if (cLives === 0){
+        gameOverWin();
+    }else{
+        computerTurnDisplay();
+        const computerTimeOut = setTimeout(computerTurnSearch, 2000);
+    }
+}
+
+//These three functions are managing the computer's game play
 function computerTurnDisplay(){
     clearTimeout(helperTimeout);
     turn = "Computer";
@@ -136,34 +154,39 @@ function computerTurnDisplay(){
     display.innerText = "Demogorgon's turn";
 }
 
-function computerSearch(){
+function computerTurnSearch(){
     if (turn !== "Computer") return;
     click1.play();
-    compOptions.sort((a,b) => 0.5 - Math.random());
-    let guess = compOptions.pop();
-    console.log(guess);
+    compHidingOptions.sort((a,b) => 0.5 - Math.random());
+    let guess = compHidingOptions.pop();
+    console.log(guess); //remove this for production!!
     compGuesses.push(guess);
-    const tile = playerTiles[guess];
+    const spot = playerTiles[guess];
     if (playerHiding.indexOf(guess) === -1){
-        tile.innerText = seeking;
-        tile.classList.add ('computerSeek');
+        spot.innerText = seeking;
+        spot.classList.add ('computerSeek');
         playerTurnDisplay();
     }else{
         pLives --;
         compFind.play();
         playerLives.innerText = pLives;
-        tile.classList.add('p-found')
-        tile.innerText = "FOUND";
-        if (pLives === 0){
-         gameOverLose();
+        spot.classList.add('p-found')
+        spot.innerText = "FOUND";
+        checkForLoss()
+    }
+}
+
+function checkForLoss(){
+    if (pLives === 0){
+        gameOverLose();    
     }else{
         playerTurnDisplay();
     }
   }
-}
 
+//These functions are determining the game over result
 function gameOverWin(){
-    music.volume = 0;
+    music.pause();
     winSound.play();
     winSound.volume= .25;
     winMusic.play();
@@ -173,12 +196,15 @@ function gameOverWin(){
 }
 
 function gameOverLose(){
-    music.volume = 0;
+    music.pause();
     loseSound.play();
+    loseMusic.loop = true;
+    loseMusic.play();
     loser.style.opacity = "1"; 
     loser.style.pointerEvents = "auto";
 }
 
+//This function is restartig the game when the restart-button is clicked
 function restart(){
     click1.play();
     window.location.reload();
